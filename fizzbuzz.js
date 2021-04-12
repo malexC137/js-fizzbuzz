8c85466efbd57db77218beff26cf390a266dc686
malexC137 <71665644+malexC137@users.noreply.github.com>
2021-04-12 22:00:21 +0200

    Ex end

---
+++ fizzbuzz.js
@@ -0,0 +1,24 @@
+//dichiaro le variabili
+var iterations = 100
+var numbers = "";
+
+//con un ciclo for creo la numerazione da 0 a 100
+for (var i = 1; i <= iterations; i++) {
+    //creo due variabili per stabilire quali numeri siano multipli di 3 e di 5
+    var fizz = i % 3 === 0
+    var buzz = i % 5 === 0 
+
+    //imposto le condizioni che il ciclo deve rispettare per eseguire la corretta numerazione
+    if ((fizz === true) && (buzz === true)) {
+        numbers += "fizzBuzz \n" 
+    } else if (fizz === true) {
+        numbers += "fizz \n"
+    } else if (buzz === true) {
+        numbers += "buzz \n"
+    } else {
+        numbers +=  i + " ";
+    }
+}
+//stampo il risultato nella console
+console.log(numbers);
+
